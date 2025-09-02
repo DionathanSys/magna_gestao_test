4a0f05d7dc2f388cf1733532b94e34bf8f51e423
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-09-02 08:02:47 -0300

    Ajuste em vinculo com plano preventivo

--- app/Filament/Resources/OrdemServicos/Actions/EncerrarOrdemServicoAction.php
+++ app/Filament/Resources/OrdemServicos/Actions/EncerrarOrdemServicoAction.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\OrdemServicos\Actions;
 
 use App\Enum\Frete\TipoRelatorioDocumentoFreteEnum;
+use App\Filament\Resources\OrdemServicos\OrdemServicoResource;
 use App\Imports\DocumentoFreteImport;
 use App\Jobs\ProcessarDocumentoFreteJob;
 use Filament\Actions\Action;
@@ -11,6 +12,7 @@ use App\Models;
 use App\Services\NotificacaoService as notify;
 
 use Filament\Forms\Components\Select;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Log;
 
 class EncerrarOrdemServicoAction
@@ -21,14 +23,16 @@ class EncerrarOrdemServicoAction
             ->label('Encerrar OS')
             ->icon('heroicon-o-check-circle')
             ->color('success')
-            ->action(function (Models\OrdemServico $record) {
+            ->action(function (Models\OrdemServico $record, Action $action) {
                 $service = new Services\OrdemServico\OrdemServicoService();
                 $service->encerrarOrdemServico($record);
                 if ($service->hasError()) {
                     notify::error(mensagem: $service->getMessage());
+                    $action->cancel();
                     return;
                 }
                 notify::success(mensagem: 'Ordem de Serviço encerrada com sucesso!');
-            });
+            })
+            ->successRedirectUrl(fn (Model $record): string => OrdemServicoResource::getUrl());
     }
 }

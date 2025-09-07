0a28ae4e6fb306078a91ead3f242024f4a5256d1
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-09-07 15:07:43 -0300

    Feature Bugio

--- app/Filament/Bugio/Pages/SolicitacaoCte.php
+++ app/Filament/Bugio/Pages/SolicitacaoCte.php
@@ -9,7 +9,7 @@ class SolicitacaoCte extends Page
 {
     protected static ?string $title = 'Solicitar CTe';
 
-    protected static string | BackedEnum | null $navigationIcon = 'heroicon-odocument-plus';
+    protected static string | BackedEnum | null $navigationIcon = 'heroicon-o-document-plus';
 
     protected string $view = 'filament.bugio.pages.solicitacao-cte';
 }

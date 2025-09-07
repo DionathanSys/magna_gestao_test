1c74a6ef25f1dda7e68f1b93e27ba27ad9a9a6ee
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-09-07 15:46:11 -0300

    Feature Bugio

--- resources/views/mail/solicitacao-cte-mail.blade.php
+++ resources/views/mail/solicitacao-cte-mail.blade.php
@@ -1,19 +1,16 @@
-<x-mail::message>
-# Solicitação de CT-e
 
-Solicitamos a emissão de CT-es para a placa *RXM2C54* referente as NF's .
+Solicitamos a emissão de CT-es para a placa *RXM2C54* referente as NF's .<br>
 
-Valor total do CT-e R$ 10.000,0 (sendo R$ 2.500,00para cada cte) 1 CT-e para cada nota / 4 CT-e no total)
+Valor total do CT-e R$ 10.000,0 (sendo R$ 2.500,00para cada cte) 1 CT-e para cada nota / 4 CT-e no total)<br>
 
-CNPJ:75.813.923/0001-61
+CNPJ:75.813.923/0001-61<br>
 
-RXM2C54
+RXM2C54<br>
 
-Juliano pedroso de campos
+Juliano pedroso de campos<br>
 
-083.144.749-40
+083.144.749-40<br>
 
 
 OBS: favor colocar frete como:1º CASO – Início no município de Água Doce para PF: ICMS DIFERIDO, CST 051 ( campo de observações: ICMS DIFERIDO CFME ARTIGO 122, INCISO II, ANEXO 6, DO RICMS/SC)<br>
 {{ config('app.name') }}
-</x-mail::message>

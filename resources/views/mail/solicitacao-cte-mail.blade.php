b4520b7bf953738dfb951077c77bc997ab95c942
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-09-11 16:04:38 -0300

    Feature Bugio

--- resources/views/mail/solicitacao-cte-mail.blade.php
+++ resources/views/mail/solicitacao-cte-mail.blade.php
@@ -1,12 +1,13 @@
 
 <p>Solicitamos a emissão de CT-es para a placa {{{$payload->veiculo}}} referente as NF's em anexo.</p>
 
-<p>Valor total frete R$ {{number_format($payload->valorFreteTotal, 2, ',', '.') }}, sendo {{$payload->quantidadeCte}} CT-e(s), R$ {{number_format($payload->valorFreteUnitario, 2, ',', '.') }} cada CT-e </p>
+<p>Valor total frete R$ {{number_format($payload->valorFreteTotal, 2, ',', '.') }}, sendo {{$payload->quantidadeCte}} CT-e(s), R$ {{number_format($payload->valorFreteUnitario, 2, ',', '.') }} cada CT-e. </p>
 
-<p>CNPJ Transportadora:75.813.923/0010-52</p>
+<p>CNPJ Transportadora: 75.813.923/0010-52</p>
 
 <p>Motorista: {{$payload->motorista['nome']}}</p>
 
 <p>CPF: {{$payload->motorista['cpf']}}</p>
 
-{{ config('app.name') }}
+
+**AxionSoft** - Magna Gestão

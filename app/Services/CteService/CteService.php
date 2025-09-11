9d28a25d1aa4e4c667ee6f6787b96c01be6038c4
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-09-11 15:25:12 -0300

    Feature Bugio

--- app/Services/CteService/CteService.php
+++ app/Services/CteService/CteService.php
@@ -20,7 +20,7 @@ class CteService
             Log::debug("dados recebidos do componente livewire", [
                 'método' => __METHOD__.'-'.__LINE__,
                 'data' => $data,
-                'db_config' => db_config('config-bugio'),
+                'db_config.valor-quilometro' => db_config('config-bugio.valor-quilometro'),
                 'user_id' => Auth::id() ?? 'N/A',
             ]);
 

c5df9fb8cf3f16190d362b07c8c807660ee76ef5
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-09-11 16:16:37 -0300

    Feature Bugio

--- app/Livewire/SolicitarCte.php
+++ app/Livewire/SolicitarCte.php
@@ -103,6 +103,7 @@ class SolicitarCte extends Component implements HasSchemas, HasActions
                             ->columnSpan(['md' => 2, 'xl' => 5])
                             ->defaultItems(1)
                             ->addActionLabel('Adicionar Integrado')
+                            ->deletable(false)
                             ->schema([
                                 Select::make('integrado_id')
                                     ->label('Integrado')

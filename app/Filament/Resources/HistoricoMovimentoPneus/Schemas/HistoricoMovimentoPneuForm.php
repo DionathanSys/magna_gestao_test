836c270b0241585fbf6af52106a2c8e3a789aad8
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-09-14 08:51:23 -0300

    Feature Pneus

--- app/Filament/Resources/HistoricoMovimentoPneus/Schemas/HistoricoMovimentoPneuForm.php
+++ app/Filament/Resources/HistoricoMovimentoPneus/Schemas/HistoricoMovimentoPneuForm.php
@@ -8,52 +8,68 @@ use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
+use Illuminate\Support\Facades\Auth;
 
 class HistoricoMovimentoPneuForm
 {
     public static function configure(Schema $schema): Schema
     {
         return $schema
+            ->columns(6)
             ->components([
                 Select::make('pneu_id')
                     ->label('Nº de Fogo')
+                    ->columnSpan(2)
                     ->relationship('pneu', 'numero_fogo')
                     ->searchable()
+                    ->disabled(fn(): bool => ! Auth::user()->is_admin)
                     ->required(),
                 Select::make('veiculo_id')
                     ->label('Veículo')
+                    ->columnSpan(2)
                     ->relationship('veiculo', 'placa')
                     ->searchable()
                     ->required(),
                 DatePicker::make('data_inicial')
                     ->label('Data Inicial')
+                    ->columnSpan(2)
                     ->required(),
                 DatePicker::make('data_final')
                     ->label('Data Final')
+                    ->columnSpan(2)
                     ->required(),
                 TextInput::make('km_inicial')
+                    ->label('KM Aplicação')
+                ->columnSpan(2)
                     ->required()
                     ->numeric(),
                 TextInput::make('km_final')
+                    ->label('KM Remoção')
+                    ->columnSpan(2)
                     ->numeric(),
                 TextInput::make('eixo')
+                    ->columnSpan(2)
                     ->required()
                     ->maxLength(255),
                 TextInput::make('posicao')
                     ->label('Posição')
+                    ->columnSpan(2)
                     ->required()
                     ->minLength(2)
                     ->maxLength(4),
                 TextInput::make('sulco_movimento')
+                    ->columnSpan(2)
                     ->required()
                     ->numeric()
                     ->default(0.00),
                 Select::make('motivo')
+                    ->columnSpan(2)
                     ->required()
                     ->options(MotivoMovimentoPneuEnum::toSelectArray()),
                 TextInput::make('ciclo_vida')
                     ->label('Vida')
                     ->required()
+                    ->columnSpan(2)
                     ->numeric()
                     ->default(0)
                     ->minValue(0)

c857ce6e5abc65dca31cad793cf84d196f31cca9
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-09-16 17:09:55 -0300

    Feature Pneus

--- app/Filament/Resources/Veiculos/RelationManagers/PneusRelationManager.php
+++ app/Filament/Resources/Veiculos/RelationManagers/PneusRelationManager.php
@@ -44,7 +44,7 @@ class PneusRelationManager extends RelationManager
                     ->relationship('pneu', 'numero_fogo')
                     ->searchable()
                     ->preload()
-                    ->required(fn(string $operation): bool => $operation === 'create'),
+                    ->required(fn(string $operation): bool => dd($operation) === 'create'),
                 TextInput::make('eixo')
                     ->visible(fn(): bool => Auth::user()->is_admin)
                     ->columnStart(1)

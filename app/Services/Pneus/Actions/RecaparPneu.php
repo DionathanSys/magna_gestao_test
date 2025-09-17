2725eb66fcaaf7647e9a32767114da750d449b8e
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-09-17 06:52:48 -0300

    Feature Pneus

--- app/Services/Pneus/Actions/RecaparPneu.php
+++ app/Services/Pneus/Actions/RecaparPneu.php
@@ -4,12 +4,14 @@ namespace App\Services\Pneus\Actions;
 
 use App\Models;
 use App\Enum;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 
 class RecaparPneu
 {
     public function handle(array $data): ?Models\Recapagem
     {
+        Log::debug(__METHOD__ . ' - Dados para recapagem do pneu', ['data' => $data]);
         $this->validate($data);
         return Models\Recapagem::create($data);
     }

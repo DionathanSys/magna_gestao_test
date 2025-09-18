dc392fab72609d27dab8841c7fdf6b6494944c4c
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-09-18 18:00:43 -0300

    Features

--- app/Filament/Resources/OrdemServicos/Actions/EditarServicoOrdemServicoAction.php
+++ app/Filament/Resources/OrdemServicos/Actions/EditarServicoOrdemServicoAction.php
@@ -19,7 +19,7 @@ class EditarServicoOrdemServicoAction
         return Action::make('editar_servico')
             ->label('Editar')
             ->icon('heroicon-o-pencil')
-            ->fillForm(fn(Action $action, array $data) => Models\ItemOrdemServico::find($recordId)->toArray())
+            ->fillForm(fn(Models\ItemOrdemServico $record): array => $record->toArray())
             ->schema(fn(Schema $schema) => ItemOrdemServicoForm::configure($schema))
             ->model(Models\ItemOrdemServico::class)
             ->action(function (array $data, string $model, Action $action) use($recordId): ?Model {

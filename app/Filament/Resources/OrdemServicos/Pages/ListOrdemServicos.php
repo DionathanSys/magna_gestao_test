a434bf89f82fd8e1861093b855bb92a73bfbdf25
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-09-19 16:08:50 -0300

    Features

--- app/Filament/Resources/OrdemServicos/Pages/ListOrdemServicos.php
+++ app/Filament/Resources/OrdemServicos/Pages/ListOrdemServicos.php
@@ -44,8 +44,8 @@ class ListOrdemServicos extends ListRecords
         return [
             'todos' => Tab::make(),
             'hoje' => Tab::make()
-                ->modifyQueryUsing(fn (Builder $query) => $query->whereDate('data_inicio', '>',now()->format('Y-m-d')))
-                ->badge(Models\OrdemServico::query()->whereDate('data_inicio', '>', now()->format('Y-m-d'))->count()),
+                ->modifyQueryUsing(fn (Builder $query) => $query->whereDate('data_inicio', '>', today()))
+                ->badge(Models\OrdemServico::query()->whereDate('data_inicio', '>', today())->count()),
             'pendente' => Tab::make()
                 ->modifyQueryUsing(fn (Builder $query) => $query->where('status', Enum\OrdemServico\StatusOrdemServicoEnum::PENDENTE)),
             'concluído' => Tab::make()

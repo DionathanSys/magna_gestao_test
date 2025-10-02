e1d2bcf037dcee685776917285621f045b2739d0
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-10-02 17:42:23 -0300

    Features

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,7 +26,7 @@ class AdminPanelProvider extends PanelProvider
     public function panel(Panel $panel): Panel
     {
         return $panel
-            // ->topNavigation()
+            ->topNavigation()
             ->maxContentWidth(Width::Full)
             ->globalSearchKeyBindings(['command+s', 'ctrl+s'])
             ->brandName('Magna Gestão')

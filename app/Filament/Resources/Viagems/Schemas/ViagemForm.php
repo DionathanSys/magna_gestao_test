48544d4a00baaa7e57610f3ff8cdd42b8381e9be
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-10-12 15:34:17 -0300

    Features

--- app/Filament/Resources/Viagems/Schemas/ViagemForm.php
+++ app/Filament/Resources/Viagems/Schemas/ViagemForm.php
@@ -73,12 +73,12 @@ class ViagemForm
                             ->default(Enum\MotivoDivergenciaViagem::DESLOCAMENTO_OUTROS->value),
                     ]),
                 Section::make('Documentos')
-                ->columns(12)
+
                     ->columnStart(1)
                     ->columnSpan(12)
                     ->schema([
                         RepeatableEntry::make('documentos')
-
+->columnSpan(12)
                             ->table([
                                 TableColumn::make('Destino'),
                                 TableColumn::make('Nº Doc.'),

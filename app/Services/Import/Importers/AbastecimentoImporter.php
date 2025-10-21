9715a075a7fd8eecb2c64eac58462f6ef14760e6
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-10-21 16:16:07 -0300

    Feature

--- app/Services/Import/Importers/AbastecimentoImporter.php
+++ app/Services/Import/Importers/AbastecimentoImporter.php
@@ -54,7 +54,7 @@ class AbastecimentoImporter implements ExcelImportInterface
             'Placa'                 => 'required|string',
             'Km'                    => 'required|numeric|min:0',
             'QtdLitros'             => 'required|numeric|min:0',
-            'VlrUnitário'           => 'required|numeric|min:0',
+            'VlrUnitrio'           => 'required|numeric|min:0',
             'VlrTotal'              => 'required|numeric|min:0',
         ], [
             'CdPrd.required'                => "A coluna 'Cód Prd' é obrigatória na linha {$rowNumber}.",
@@ -65,7 +65,7 @@ class AbastecimentoImporter implements ExcelImportInterface
             'Placa.required'                => "A coluna 'Placa' é obrigatória na linha {$rowNumber}.",
             'Km.required'                   => "A coluna 'Km' é obrigatória na linha {$rowNumber}.",
             'QtdLitros.required'            => "A coluna 'Qtd Litros' é obrigatória na linha {$rowNumber}.",
-            'VlrUnitário.required'          => "A coluna 'Vlr Unitário' é obrigatória na linha {$rowNumber}.",
+            'VlrUnitrio.required'          => "A coluna 'Vlr Unitário' é obrigatória na linha {$rowNumber}.",
             'VlrTotal.required'             => "A coluna 'Vlr Total' é obrigatória na linha {$rowNumber}.",
         ]);
 

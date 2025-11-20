3703f2aa370a6f4768b236743d31a250c3f30b38
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-11-20 11:49:51 -0300

    Feature

--- app/Models/ResultadoPeriodo.php
+++ app/Models/ResultadoPeriodo.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ResultadoPeriodo extends Model
 {
-    //
+    public function veiculo(): BelongsTo
+    {
+        return $this->belongsTo(Veiculo::class);
+    }
+
+    public function tipoVeiculo(): BelongsTo
+    {
+        return $this->belongsTo(TipoVeiculo::class, 'tipo_veiculo_id');
+    }
 }

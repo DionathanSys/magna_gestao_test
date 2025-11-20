7280c6091f3a008ae7bf97cbff22d90dcb0cfb1f
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-11-20 11:41:14 -0300

    Feature

--- app/Enum/ClienteEnum.php
+++ app/Enum/ClienteEnum.php
@@ -4,11 +4,13 @@ namespace App\Enum;
 
 enum ClienteEnum: string
 {
-    case BRF_CCO = 'BRF S.A. CHAPECO/SC';
-    case BRF_CTV = 'BRF S.A. CATANDUVAS/SC';
-    case BRF_FBT = 'BRF S.A. FRANCISCO BELTRÃO/PR';
-    case BRF_CNC = 'BRF S.A. CONCÓRDIA/SC';
-    case BUGIO   = 'Bugio';
+    case BRF_CCO        = 'BRF S.A. CHAPECO/SC';
+    case BRF_CTV        = 'BRF S.A. CATANDUVAS/SC';
+    case BRF_FBT        = 'BRF S.A. FRANCISCO BELTRÃO/PR';
+    case BRF_CNC        = 'BRF S.A. CONCÓRDIA/SC';
+    case BUGIO          = 'Bugio';
+    case NUTREPAMPA_RS  = 'Nutrepampa RS';
+    case NUTREPAMPA_SC  = 'Nutrepampa SC';
 
     public static function toSelectArray(): array
     {

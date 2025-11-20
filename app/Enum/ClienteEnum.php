9701362ad4d44f52f2c232a9ad612bb2dda6f52c
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-11-20 11:15:16 -0300

    Feature

--- app/Enum/ClienteEnum.php
+++ app/Enum/ClienteEnum.php
@@ -4,10 +4,10 @@ namespace App\Enum;
 
 enum ClienteEnum: string
 {
-    case BRF_CCO = 'BRF Chapecó';
-    case BRF_CTV = 'BRF Catanduvas';
-    case BRF_FBT = 'BRF Francisco Beltrão';
-    case BRF_CNC = 'BRF Concórdia';
+    case BRF_CCO = 'BRF S.A. CHAPECO/SC';
+    case BRF_CTV = 'BRF S.A. CATANDUVAS/SC';
+    case BRF_FBT = 'BRF S.A. FRANCISCO BELTRÃO/PR';
+    case BRF_CNC = 'BRF S.A. CONCÓRDIA/SC';
     case BUGIO   = 'Bugio';
 
     public static function toSelectArray(): array
--- app/Filament/Resources/Veiculos/Schemas/VeiculoForm.php
+++ app/Filament/Resources/Veiculos/Schemas/VeiculoForm.php
@@ -6,6 +6,7 @@ use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Components\Tabs;
 use Filament\Schemas\Schema;
 
@@ -21,73 +22,83 @@ class VeiculoForm
                         Tabs\Tab::make('Geral')
                             ->columns(12)
                             ->schema([
-                                TextInput::make('placa')
-                                    ->columnSpan(1)
-                                    ->disabledOn('edit')
-                                    ->required(),
-                                Select::make('filial')
-                                    ->columnSpan(2)
-                                    ->options([
-                                        'CATANDUVAS' => 'Catanduvas',
-                                        'CHAPECO'    => 'Chapecó',
-                                        'CONCORDIA'  => 'Concórdia',
+                                Section::make('')
+                                    ->columns(12)
+                                    ->columnSpanFull()
+                                    ->schema([
+                                        TextInput::make('placa')
+                                            ->columnSpan(1)
+                                            ->disabledOn('edit')
+                                            ->required(),
+                                        Select::make('filial')
+                                            ->columnSpan(2)
+                                            ->options([
+                                                'CATANDUVAS' => 'Catanduvas',
+                                                'CHAPECO'    => 'Chapecó',
+                                                'CONCORDIA'  => 'Concórdia',
+                                            ])
+                                            ->required(),
+                                        Toggle::make('is_active')
+                                            ->label('Ativo')
+                                            ->columnSpan(1)
+                                            ->inline(false)
+                                            ->default(true)
+                                            ->required(),
+                                        TextInput::make('marca')
+                                            ->label('Marca')
+                                            ->columnSpan(2)
+                                            ->maxLength(50)
+                                            ->placeholder('Marca do veículo'),
+                                        TextInput::make('modelo')
+                                            ->label('Modelo')
+                                            ->columnSpan(2)
+                                            ->maxLength(50)
+                                            ->placeholder('Modelo do veículo'),
+                                        TextInput::make('chassis')
+                                            ->label('Chassi')
+                                            ->columnSpan(2)
+                                            ->maxLength(50)
+                                            ->placeholder('Chassi do veículo'),
+                                        Select::make('tipo_veiculo_id')
+                                            ->label('Tipo de Veículo')
+                                            ->columnStart(1)
+                                            ->columnSpan(3)
+                                            ->searchable()
+                                            ->relationship('tipoVeiculo', 'descricao')
+                                            ->required(),
+                                    ]),
+                                Section::make('Informações Complementares')
+                                    ->columns(12)
+                                    ->columnSpanFull()
+                                    ->compact()
+                                    ->schema([
+                                        TextInput::make('informacoes_complementares.codigo_imobilizado')
+                                            ->label('Código Imobilizado')
+                                            ->columnSpan(2),
+                                        Select::make('informacoes_complementares.bloqueador_silo')
+                                            ->label('Tipo Bloqueador de Silo')
+                                            ->columnSpan(2)
+                                            ->options([
+                                                'NENHUM'     => 'Nenhum',
+                                                'HOMOLOGADO' => 'Homologado',
+                                                'RELE'       => 'Relé',
+                                            ]),
+                                        DatePicker::make('informacoes_complementares.afericao_tacografo')
+                                            ->label('Dt. Próx. Aferição Tacógrafo')
+                                            ->columnSpan(2),
+                                        DatePicker::make('informacoes_complementares.teste_fumaca')
+                                            ->label('Dt. Teste de Fumaça')
+                                            ->columnSpan(2),
+                                        DatePicker::make('informacoes_complementares.data_ultimo_checklist')
+                                            ->label('Dt. Último Checklist')
+                                            ->columnSpan(2),
                                     ])
-                                    ->required(),
-                                Toggle::make('is_active')
-                                    ->label('Ativo')
-                                    ->columnSpan(1)
-                                    ->inline(false)
-                                    ->default(true)
-                                    ->required(),
-                                TextInput::make('marca')
-                                    ->label('Marca')
-                                    ->columnSpan(2)
-                                    ->maxLength(50)
-                                    ->placeholder('Marca do veículo'),
-                                TextInput::make('modelo')
-                                    ->label('Modelo')
-                                    ->columnSpan(2)
-                                    ->maxLength(50)
-                                    ->placeholder('Modelo do veículo'),
-                                TextInput::make('chassis')
-                                    ->label('Chassi')
-                                    ->columnSpan(2)
-                                    ->maxLength(50)
-                                    ->placeholder('Chassi do veículo'),
-                                Select::make('tipo_veiculo_id')
-                                    ->label('Tipo de Veículo')
-                                    ->columnStart(1)
-                                    ->columnSpan(3)
-                                    ->searchable()
-                                    ->relationship('tipoVeiculo', 'descricao')
-                                    ->required(),
+
                             ])
                             ->icon('heroicon-o-information-circle'),
                         Tabs\Tab::make('Info Complementares')
                             ->columns(12)
-                            ->schema([
-                                TextInput::make('informacoes_complementares.codigo_imobilizado')
-                                    ->label('Código Imobilizado')
-                                    ->columnSpan(2),
-                                Select::make('informacoes_complementares.bloqueador_silo')
-                                    ->label('Tipo Bloqueador de Silo')
-                                    ->columnSpan(2)
-                                    ->options([
-                                        'NENHUM'     => 'Nenhum',
-                                        'HOMOLOGADO' => 'Homologado',
-                                        'RELE'       => 'Relé',
-                                    ]),
-                                DatePicker::make('informacoes_complementares.afericao_tacografo')
-                                    ->label('Dt. Próx. Aferição Tacógrafo')
-                                    ->columnSpan(2),
-                                DatePicker::make('informacoes_complementares.teste_fumaca')
-                                    ->label('Dt. Teste de Fumaça')
-                                    ->columnSpan(2),
-                                DatePicker::make('informacoes_complementares.data_ultimo_checklist')
-                                    ->label('Dt. Último Checklist')
-                                    ->columnSpan(2),
-                                
-                            ])
+
                     ])
                     ->columnSpanFull(),
             ]);

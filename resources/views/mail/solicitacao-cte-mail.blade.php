7baf1adcbb2a756eafdcd01e4f5b86be5b46e8db
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-11-21 14:07:24 -0300

    Feature

--- resources/views/mail/solicitacao-cte-mail.blade.php
+++ resources/views/mail/solicitacao-cte-mail.blade.php
@@ -17,7 +17,7 @@
 
     <p>Valor total frete R$ {{number_format($payload->valorFreteTotal, 2, ',', '.') }}, sendo {{$payload->quantidadeCte}} CT-e(s), R$ {{number_format($payload->valorFreteUnitario, 2, ',', '.') }} cada CT-e. </p>
 
-    <p>CNPJ Transportadora: 75.813.923/0010-52</p>
+    <p>CNPJ Transportadora: 75.813.923/0001-61</p>
 
     <p>Motorista: {{$payload->motorista['nome']}}</p>
 
@@ -25,6 +25,9 @@
 
     <h4>Observações:</h4>
     <p>Início no município de Chapecó para PF: ICMS DIFERIDO, CST 051 ( campo de observações: ICMS DIFERIDO CFME ARTIGO 122, INCISO II, ANEXO 6, DO RICMS/SC)</p>
+    <p>Remetente: Bugio Nutrição – 50.593.076/0001-46</p>
+    <p>Destinatário: Nildo Bes – 774.033.719-68</p>
+    <p>Tomador: Bugio Agropecuária – 82.996.521/0001-05</p>
 
     <p>Favor responder este e-mail, incluindo todos os destinatários em cópia.</p>
     <p>Obrigado!</p>

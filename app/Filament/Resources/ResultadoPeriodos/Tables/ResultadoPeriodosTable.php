26d36545a53cdbb7f7cf12915fb6ad6877643427
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-11-21 14:41:36 -0300

    Feature

--- app/Filament/Resources/ResultadoPeriodos/Tables/ResultadoPeriodosTable.php
+++ app/Filament/Resources/ResultadoPeriodos/Tables/ResultadoPeriodosTable.php
@@ -23,9 +23,9 @@ class ResultadoPeriodosTable
         return $table
             ->modifyQueryUsing(function ($query) {
                 $query = $query->with(['veiculo:id,placa', 'tipoVeiculo:id,descricao', 'abastecimentoInicial', 'abastecimentoFinal']);
-                return $query->withSum('documentosFrete as documentos_frete', 'valor_liquido')
-                    ->withSum('viagens', 'km_pago')
-                    ->withSum('abastecimentos', 'preco_total');
+                // return $query->withSum('documentosFrete as documentos_frete', 'valor_liquido')
+                //     ->withSum('viagens', 'km_pago')
+                //     ->withSum('abastecimentos', 'preco_total');
             })
             ->columns([
                 TextColumn::make('veiculo.placa')
@@ -60,13 +60,10 @@ class ResultadoPeriodosTable
                     ->width('1%')
                     ->numeric(0, ',', '.')
                     ->sortable(),
-                TextColumn::make('documentos_frete_sum_valor_liquido')
+                TextColumn::make('documentosFrete_sum_valor_liquido')
                 ->label('Receita')
                 ->money('BRL')
-                ->summarize(Sum::make()
-                    ->money('BRL')
-                    ->label('Total Receita')
-                ),
+                ->sum('documentosFrete', 'valor_liquido'), // Filament cuida da conversão
 
             TextColumn::make('viagens_sum_km_pago')
                 ->label('KM Pago')

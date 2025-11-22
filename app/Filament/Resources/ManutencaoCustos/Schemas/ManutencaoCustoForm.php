3703af4aa329733e5cb8407779f54c736602bdc3
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-11-22 11:47:50 -0300

    Feature

--- app/Filament/Resources/ManutencaoCustos/Schemas/ManutencaoCustoForm.php
+++ app/Filament/Resources/ManutencaoCustos/Schemas/ManutencaoCustoForm.php
@@ -24,7 +24,7 @@ class ManutencaoCustoForm
                     ->searchable()
                     ->live(onBlur: true)
                     ->afterStateUpdated(function (Set $set, $state) {
-                        if($state){
+                        if ($state) {
                             $resultadoPeriodo = Models\ResultadoPeriodo::where('veiculo_id', $state)
                                 ->where('status', StatusDiversosEnum::PENDENTE->value)
                                 ->first();
@@ -33,17 +33,17 @@ class ManutencaoCustoForm
                         }
                         $set('resultado_periodo_id', null);
                     }),
+                TextInput::make('custo_total')
+                    ->label('Custo Total R$')
+                    ->prefix('R$')
+                    ->required()
+                    ->numeric(),
                 DatePicker::make('data_inicio')
                     ->label('Data Início')
                     ->required(),
                 DatePicker::make('data_fim')
                     ->label('Data Fim')
                     ->required(),
-                TextInput::make('custo_total')
-                    ->label('Custo Total R$')
-                    ->prefix('R$')
-                    ->required()
-                    ->numeric(),
                 TextInput::make('resultado_periodo_id')
                     ->label('Resultado Período ID')
                     ->required()

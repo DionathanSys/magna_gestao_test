469869900712c37b114c5744df119c1bbd9c14ed
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-11-24 03:06:19 -0300

    Feature

--- app/Filament/Bugio/Resources/ViagemBugios/Tables/ViagemBugiosTable.php
+++ app/Filament/Bugio/Resources/ViagemBugios/Tables/ViagemBugiosTable.php
@@ -6,6 +6,7 @@ use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Support\Facades\Auth;
 
@@ -15,7 +16,39 @@ class ViagemBugiosTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('id')
+                    ->label('ID')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('veiculo.placa')
+                    ->label('Placa')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('destinos.integrado_nome')
+                    ->label('Integrados')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('data_competencia')
+                    ->label('Data Viagem')
+                    ->date('d/m/Y')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('km_pago')
+                    ->label('Km Pago')
+                    ->numeric(0, ',', '.')
+                    ->sortable(),
+                TextColumn::make('frete')
+                    ->label('Frete')
+                    ->money('BRL')
+                    ->sortable(),
+                TextColumn::make('condutor')
+                    ->label('Motorista')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('status')
+                    ->label('Status')
+                    ->sortable()
+                    ->searchable(),
             ])
             ->filters([
                 //

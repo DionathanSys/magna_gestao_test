e29b00ced07588de33ac3c19c322e68a21253fd5
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-11-24 17:09:38 -0300

    Feature

--- app/Filament/Resources/ResultadoPeriodos/ResultadoPeriodoResource.php
+++ app/Filament/Resources/ResultadoPeriodos/ResultadoPeriodoResource.php
@@ -25,8 +25,6 @@ class ResultadoPeriodoResource extends Resource
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
 
-    protected static ?string $recordTitleAttribute = 'title';
-
     public static function form(Schema $schema): Schema
     {
         return ResultadoPeriodoForm::configure($schema);

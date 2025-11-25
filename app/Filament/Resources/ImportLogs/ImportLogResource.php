1b9a17815127b1bfda80a47a92ee00cf47cc7f0e
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-11-25 11:47:06 -0300

    Feature

--- app/Filament/Resources/ImportLogs/ImportLogResource.php
+++ app/Filament/Resources/ImportLogs/ImportLogResource.php
@@ -20,6 +20,8 @@ class ImportLogResource extends Resource
 {
     protected static ?string $model = ImportLog::class;
 
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    
     protected static ?string $modelLabel = 'Log Importação';
 
     protected static ?string $pluralModelLabel = 'Log Importações';

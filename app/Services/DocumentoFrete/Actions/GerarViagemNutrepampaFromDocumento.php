88404740c5f1a328a34e031bfac64b0834922c79
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-11-30 22:44:15 -0300

    Feature

--- app/Services/DocumentoFrete/Actions/GerarViagemNutrepampaFromDocumento.php
+++ app/Services/DocumentoFrete/Actions/GerarViagemNutrepampaFromDocumento.php
@@ -19,6 +19,8 @@ class GerarViagemNutrepampaFromDocumento
     public function handle()
     {
         $this->processarDocumentosFrete();
+
+        //TODO usar valores de R$/km para identifcar km pago
     }
 
     private function processarDocumentosFrete(): void
@@ -31,13 +33,19 @@ class GerarViagemNutrepampaFromDocumento
 
             $documentosGroupDataEmissao->each(function (Collection $documentosDataEmissao, $dataEmissao) use ($veiculoId) {
 
-                $documentos = $this->separarEmSequencias($documentosDataEmissao);
+                $documentosDataEmissao->groupBy('valor_total')->each(function (Collection $documentosGroupValorTotal, $valorTotal) use ($veiculoId, $dataEmissao) {
 
-                $this->data[$veiculoId . '.' . $dataEmissao] = [
-                    'veiculo_id' => $veiculoId,
-                    'data_emissao' => $dataEmissao,
-                    'documentos_frete' => $documentos,
-                ];
+                    $documentos = $documentosGroupValorTotal->toArray();
+
+                    $this->data[] = [
+                        'veiculo_id' => $veiculoId,
+                        'data_emissao' => $dataEmissao,
+                        'valor_total' => $valorTotal,
+                        'documentos_frete_ids' => $documentos,
+                    ];
+
+                });
+               
             });
 
         });
@@ -49,49 +57,6 @@ class GerarViagemNutrepampaFromDocumento
 
     }
 
-    private function separarEmSequencias(Collection $documentos): array
-    {
-
-        Log::debug('Separando documentos em sequências.', [
-            'metodo' => __METHOD__ . '@' . __LINE__,
-            'documentos_ids' => $documentos->pluck('numero_documento', 'id')->toArray(),
-        ]);
-
-        if ($documentos->isEmpty()) {
-            return [];
-        }
-
-        $sequencias = [];
-        $sequenciaAtual = [];
-
-        foreach ($documentos as $index => $doc) {
-            if (empty($sequenciaAtual)) {
-                // Primeira documento da sequência
-                $sequenciaAtual[] = $doc;
-                continue;
-            }
-
-            $ultimoDoc = end($sequenciaAtual);
-            $numeroAtual = (int) $doc->numero_documento;
-            $numeroAnterior = (int) $ultimoDoc->numero_documento;
-
-            // Verificar se é sequencial (diferença de 1)
-            if ($numeroAtual === $numeroAnterior + 1) {
-                // Continua a sequência
-                $sequenciaAtual[] = $doc;
-            } else {
-                // Quebra a sequência
-                $sequencias[] = $sequenciaAtual;
-                $sequenciaAtual = [$doc];
-            }
-        }
-
-        // Adicionar última sequência
-        if (!empty($sequenciaAtual)) {
-            $sequencias[] = $sequenciaAtual;
-        }
-
-        return $sequencias;
-    }
+   
 
 }

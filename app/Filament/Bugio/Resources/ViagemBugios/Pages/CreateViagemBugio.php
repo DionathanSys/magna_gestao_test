dab9092bbd89ec307177699e33a5efc7b4c9dfd7
DionathanSys <150610527+DionathanSys@users.noreply.github.com>
2025-12-25 08:23:00 -0300

    ajustes form solicitação CTe

--- app/Filament/Bugio/Resources/ViagemBugios/Pages/CreateViagemBugio.php
+++ app/Filament/Bugio/Resources/ViagemBugios/Pages/CreateViagemBugio.php
@@ -22,7 +22,7 @@ class CreateViagemBugio extends CreateRecord
         $data['veiculo_id']         = Veiculo::query()->where('placa', $data['veiculo'])->value('id');
         $data['km_pago']            = $data['km_total'] ?? 0;
         $data['km_rodado']          = 0;
-        $data['data_competencia']   = $data['data_competencia'] ? Carbon::parse($data['data_competencia'])->format('Y-m-d') : now()->format('Y-m-d');
+        $data['data_competencia']   = $data['data_competencia'] ? Carbon::createFromFormat('d/m/Y', $data['data_competencia'])->format('Y-m-d') : now()->format('Y-m-d');
         $data['frete']              = $data['valor_frete'] ?? 0.0;
         $data['condutor']           = collect(db_config('config-bugio.motoristas'))->firstWhere('cpf', $data['motorista'] ?? null)['motorista'] ?? null;
         $data['motorista']          = [

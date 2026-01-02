2dbd53d78c49c498a30cee20b1cef3dc5264e47b
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2026-01-02 17:48:16 -0300

    fix

--- resources/views/mail/solicitacao-cte-mail.blade.php
+++ resources/views/mail/solicitacao-cte-mail.blade.php
@@ -13,28 +13,30 @@
     </style>
 </head>
 <body>
-    <p>Solicitamos a emissão de CT-e para a placa {{{$payload->veiculo}}} referente as NF's ({{is_array($payload->nro_notas) ? implode(', ', $payload->nro_notas) : $payload->nro_notas}}) em anexo.</p>
+    {{-- <p>Solicitamos a emissão de CT-e para a placa {{{$payload->veiculo}}} referente as NF's ({{is_array($payload->nro_notas) ? implode(', ', $payload->nro_notas) : $payload->nro_notas}}) em anexo.</p> --}}
 
-     @if ($payload->cte_retroativo)
-        <p>CTe Retroativo</p>
-    @endif 
+     {{-- @if ($payload->cte_retroativo) --}}
+        {{-- <p>CTe Retroativo</p> --}}
+    {{-- @endif  --}}
 
-    @if ($payload->cte_complementar)
-        <p>Complementar ao CT-e: {{$payload->cte_referencia}}</p>
-    @endif
+    {{-- @if ($payload->cte_complementar) --}}
+        {{-- <p>Complementar ao CT-e: {{$payload->cte_referencia}}</p> --}}
+    {{-- @endif --}}
 
-    <p>Valor total frete R$ {{number_format($payload->valorFreteTotal, 2, ',', '.') }}, sendo {{$payload->quantidadeCte}} CT-e(s), R$ {{number_format($payload->valorFreteUnitario, 2, ',', '.') }} cada CT-e. </p>
+    {{-- <p>Valor total frete R$ {{number_format($payload->valorFreteTotal, 2, ',', '.') }}, sendo {{$payload->quantidadeCte}} CT-e(s), R$ {{number_format($payload->valorFreteUnitario, 2, ',', '.') }} cada CT-e. </p> --}}
 
     <p>CNPJ Transportadora: 75.813.923/0001-61</p>
 
-    <p>Motorista: {{$payload->motorista['nome']}}</p>
+    {{-- <p>Motorista: {{$payload->motorista['nome']}}</p> --}}
 
-    <p>CPF: {{$payload->motorista['cpf']}}</p>
+    {{-- <p>CPF: {{$payload->motorista['cpf']}}</p> --}}
 
     <h4>Observações:</h4>
     <p>Início no município de Chapecó para PF: ICMS DIFERIDO, CST 051 (campo de observações: ICMS DIFERIDO CFME ARTIGO 122, INCISO II, ANEXO 6, DO RICMS/SC)</p>
     <p>Remetente: Bugio Nutrição – 50.593.076/0001-46</p>
-    
+    @foreach ($payload->destinos as $key => $destino)
+        <p>Destinatário: {{ $destino['integrado_nome'] }}</p>
+    @endforeach
     <p>Tomador: Bugio Agropecuária – 82.996.521/0001-05</p>
 
     <p>Favor responder este e-mail, incluindo todos os destinatários em cópia.</p>

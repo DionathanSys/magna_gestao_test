3fc44610c4275bcae65e35dda9940b6b05c860e8
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2026-01-02 10:56:58 -0300

    fix

--- app/Filament/Bugio/Resources/ViagemBugios/Pages/CreateViagemBugio.php
+++ app/Filament/Bugio/Resources/ViagemBugios/Pages/CreateViagemBugio.php
@@ -55,7 +55,7 @@ class CreateViagemBugio extends CreateRecord
             'updated_by' => $result->updated_by,
         ]);
 
-        Log::debug($anexos, $result);
+        Log::debug('r', [$anexos, $result]);
 
         return $result;
     }

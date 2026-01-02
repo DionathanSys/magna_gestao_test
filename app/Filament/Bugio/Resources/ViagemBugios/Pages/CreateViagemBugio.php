91593bf1bb31b886654743219dbf7956b2e781aa
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2026-01-02 12:08:32 -0300

    fix

--- app/Filament/Bugio/Resources/ViagemBugios/Pages/CreateViagemBugio.php
+++ app/Filament/Bugio/Resources/ViagemBugios/Pages/CreateViagemBugio.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Bugio\Resources\ViagemBugios\Pages;
 
+use App\Enum\Frete\TipoDocumentoEnum;
 use App\Filament\Bugio\Resources\ViagemBugios\ViagemBugioResource;
 use App\Jobs\SolicitarCteBugio;
 use App\Models\Integrado;
@@ -26,9 +27,9 @@ class CreateViagemBugio extends CreateRecord
         $data['km_pago']            = $data['km_total'] ?? 0;
         $data['km_rodado']          = 0;
         $data['data_competencia']   = $data['data_competencia'] ? Carbon::createFromFormat('d/m/Y', $data['data_competencia'])->format('Y-m-d') : now()->format('Y-m-d');
-        $data['frete']              = $data['valor_frete'] ?? 0.0;
+        $data['frete']              = $this->calcularFrete($data['km_total']);
         $data['condutor']           = collect(db_config('config-bugio.motoristas'))->firstWhere('cpf', $data['motorista'] ?? null)['motorista'] ?? null;
-        $data['motorista']          = [
+        $data['info_adicionais']['motorista'] = [
             'cpf' => $data['motorista'] ?? null,
         ];
         $data['created_by']         = Auth::id();
@@ -55,13 +56,46 @@ class CreateViagemBugio extends CreateRecord
             'updated_by' => $result->updated_by,
         ]);
 
-        SolicitarCteBugio::dispatch($data);
+        $this->solicitarCte($data);
 
         return $result;
     }
 
     protected function solicitarCte(array $data)
     {
-        
+        $data = [
+            'km_total'          => $data['km_total'],
+            'valor_frete'       => $data['frete'],
+            'motorista'         => [
+                'cpf' => $data['info_adicionais']['motorista'],
+            ],
+            'veiculo'           => $data['veiculo'],
+            'nro_notas'         => $data['nro_notas'],
+            'cte_retroativo'    => $data['info_adicionais']['cte_retroativo'] ?? false,
+            'cte_complementar'  => $data['tipo_documento'] == TipoDocumentoEnum::CTE_COMPLEMENTO->value,
+            'destinos'          => [
+                $data['destinos']
+            ],
+            'veiculo_id'        => $data['veiculo_id'],
+            'km_pago'           => $data['km_pago'],
+            'km_rodado'         => $data['km_rodado'],
+            'veiculo_id'        => $data['veiculo_id'],
+            'status'            => 'pendente',
+            'created_by'        => $data['created_by'],
+            'updated_by'        => $data['updated_by'],
+            'condutor'          => $data['condutor'],
+
+        ];
+
+        dd($data);
+
+        SolicitarCteBugio::dispatch($data);
+    }
+
+    private function calcularFrete(float $kmTotal): float
+    {
+        $valorQuilometro = db_config('config-bugio.valor-quilometro', 0);
+
+        return $valorQuilometro * $kmTotal;
     }
 }

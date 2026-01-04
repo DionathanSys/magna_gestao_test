63ecb2368388eb88e22c569ed56f1b7e96622998
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2026-01-04 06:57:35 -0300

    fix

--- app/Filament/Bugio/Resources/ViagemBugios/Schemas/ViagemBugioForm.php
+++ app/Filament/Bugio/Resources/ViagemBugios/Schemas/ViagemBugioForm.php
@@ -21,7 +21,7 @@ class ViagemBugioForm
         return $schema
             ->components([
                 Grid::make()
-                    ->columns(2)
+                    ->columns(['lg' => 2])
                     ->columnSpanFull()
                     ->schema([
                         Section::make('Detalhes da Viagem')

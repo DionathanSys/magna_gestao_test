ce063b5a8ddb86b152317fc4ad332bc3b8946623
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2026-01-25 08:49:27 -0300

    fix

--- app/Filament/Resources/Viagems/Tables/ViagemsTable.php
+++ app/Filament/Resources/Viagems/Tables/ViagemsTable.php
@@ -536,7 +536,7 @@ class ViagemsTable
                                 'created_by' => Auth::id(),
                                 'updated_by' => Auth::id(),
                             ]);
-                            $livewire->dispatch('close-action-group');
+                            $livewire->dispatch(('$refresh'));
                         })
                         ->hidden(fn(Models\Viagem $record): bool => $record->cargas_count > 0 || $record->documentos_count > 0)
                         ->color('danger'),

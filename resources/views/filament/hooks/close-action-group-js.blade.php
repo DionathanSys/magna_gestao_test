c67f3e5df6a0ade36b3d640ed060e879db1a67eb
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2026-01-25 14:28:08 -0300

    fix

--- resources/views/filament/hooks/close-action-group-js.blade.php
+++ resources/views/filament/hooks/close-action-group-js.blade.php
@@ -1,13 +1,6 @@
 <script>
-    document.addEventListener('alpine:init', () => {
-        window.addEventListener('filament-close-action-group', () => {
-            document
-                .querySelectorAll('[x-data*="isOpen"]')
-                .forEach(el => {
-                    if (el.__x?.$data?.isOpen !== undefined) {
-                        el.__x.$data.isOpen = false;
-                    }
-                });
-        });
-    });
+    window.addEventListener('filament-close-action-group', () => {
+        // força fechamento simulando click fora
+        document.body.click()
+    })
 </script>
